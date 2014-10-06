a118b172d7003dd0bc7fe024e8be0179d41590ce
Casey Yee <caseyyee.ca@gmail.com>
2014-10-06 11:47:47 -0700

    switched to slimerjs

--- hiro-ui/gulpfile.js
+++ hiro-ui/gulpfile.js
@@ -16,7 +16,6 @@ gulp.task('styles', function() {
     .pipe(gulp.dest('build/css'));
 });
 
-
 gulp.task('markdown', function() {
   gulp.src(['src/docs/*.md'])
     .pipe(markdown())
@@ -33,21 +32,37 @@ gulp.task('content', function() {
     .pipe(gulp.dest('build'))
 });
 
-gulp.task('bowercopy', function() {
+gulp.task('bower', function() {
+  gulp.src(['!bower_components/fira{,/**}','bower_components/**/*.*'])
+    .pipe(gulp.dest('build/js'));
+})
+
+
+// copy fira font into appropriate folder
+gulp.task('font', function() {
   gulp.src(['bower_components/fira/**/*.*'])
     .pipe(gulp.dest('build/fonts'));
+})
+
+gulp.task('copy', function() {
+  gulp.src(['src/data/*.*'],{ base: './src' })
+    .pipe(gulp.dest('build'));
 })
 
 
 
 gulp.task('default', function() {
-  gulp.run('scripts', 'styles', 'content', 'bowercopy');
+  gulp.run('scripts', 'styles', 'content', 'font', 'copy', 'bower');
 
-  gulp.watch('src/sass/**', function(event) {
+  gulp.watch('src/sass/**/*.*', function(event) {
     gulp.run('styles');
   });
-  gulp.watch('src/jade/**', function(event) {
+  gulp.watch('src/jade/**/*.*', function(event) {
     gulp.run('content');
   });
+  gulp.watch('src/scripts/**/*.*', function(event) {
+    gulp.run('scripts');
+  });
+
 
 });
