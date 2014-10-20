204b852275965ee5ba29f8882f9bad3479837a98
Casey Yee <caseyyee.ca@gmail.com>
2014-10-20 16:47:02 -0700

    added build files for gh-pages

---
+++ hiro-ui/build/js/title.js
@@ -0,0 +1,13 @@
+(function() {
+	var uidata = document.querySelector('#uidata');
+
+	var d23 = new DOM2three('./data/title.json');
+	d23.onload = function(items) {
+		// apply content to HTML layout.
+		this.applyContent(document.body);
+		// calculate rectangles for each layout item.
+		this.getItemsRectangles();
+		// insert resultant JSON data onto page for the renderer to pick up.
+		uidata.innerHTML = JSON.stringify(this.data);
+	}
+})();
